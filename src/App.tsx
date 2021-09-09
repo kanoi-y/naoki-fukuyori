48c98fe34d56974d2f0e6845caf00d39a7dca4ea
kanoi-y <furafuratonbo@gmail.com>
2021-09-09 22:04:06 +0900

    TopViewのUIを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import { TopView } from "components/organisms/TopView";
 
-import "./App.css";
-import { TopView } from "./components/organisms/TopView";
+// const theme = extendTheme({
+//   fonts: {
+//     heading: "righteous",
+//   },
+// });
 
 function App() {
   return (
     <ChakraProvider>
-      <Box bg="#f2f2f2">
+      <Box bg="#f2f2f2" w="100%" h="100vh">
         <TopView />
       </Box>
     </ChakraProvider>
--- src/components/organisms/TopView.tsx
+++ src/components/organisms/TopView.tsx
@@ -1,11 +1,28 @@
-import { Text } from "@chakra-ui/layout";
-import { Flex } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/layout";
+import { Flex, Image } from "@chakra-ui/react";
+import "@fontsource/righteous";
+import AngleDoubleDown from "images/angle-double-down-solid.svg";
 import { memo, VFC } from "react";
 
 export const TopView: VFC = memo(() => {
   return (
-    <Flex>
-      <Text>Welcome</Text>
+    <Flex
+      w="100%"
+      h="100%"
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Heading
+        fontSize="45px"
+        fontWeight="bold"
+        fontStyle="italic"
+        mb="40px"
+        style={{ fontFamily: "Righteous" }}
+      >
+        Welcome
+      </Heading>
+      <Image src={AngleDoubleDown} w="50px" opacity="0.6" />
     </Flex>
   );
 });
